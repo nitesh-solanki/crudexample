8dbc941d448ef802ded817a24823b8e78f152ecf
nitesh-solanki <er.nitesh11@gmail.com>
2019-11-05 11:47:04 +0530

    final changes

--- crudexample/urls.py
+++ crudexample/urls.py
@@ -20,7 +20,7 @@ from employee import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emp', views.emp),
-    path('show/', views.show),
+    path('show', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
     path('delete/<int:id>', views.destroy),
